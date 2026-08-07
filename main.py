b3c0340248c099edf1580c751d6b95fbf6beb2a7
Ali <131938587+AliixDev@users.noreply.github.com>
2026-08-07 18:47:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api")
-def read_root():
-    return {"message": "Api Ok "}
+
     @app.get("/api/items")
 def get_products():
     return ["Laptop", "Mouse", "Keyboard", "Monitor"]
